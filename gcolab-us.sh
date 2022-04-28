2440e4ad96079ce5080f78d5eb0660e26f580952
lilbuno1 <102577859+lilbuno1@users.noreply.github.com>
2022-04-27 20:12:19 -0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -1,12 +1,3 @@
-#! /bin/bash
-# Install XRDP
-# Before get xmr coin for free
-# Google Colab
-sudo apt update
-clear
-sudo apt install screen
-screen -R xmr
- 
 wget wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-bionic-x64.tar.gz
 tar xvzf xmrig-6.17.0-bionic-x64.tar.gz
 cd xmrig-6.17.0
